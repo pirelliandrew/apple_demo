d341f60f07fdd3a0ae0fca6a10b6a68fb98dcc06
pirelliandrew <pirelliandrew@gmail.com>
2023-04-08 16:25:45 -0700

    Add weather forecast details section to page and specs

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -2,50 +2,77 @@ require 'rails_helper'
 
 RSpec.describe "Weather", type: :request do
   describe "GET /weather" do
+    before { get "/weather", params: }
+
+    let(:parsed_html_body) { Nokogiri::HTML(response.body).css('body') }
+
     shared_examples "a page with an address form" do
+      let(:address_form) do
+        parsed_html_body.css('form[action="/weather"][accept-charset="UTF-8"][method="get"]')
+      end
+
       it "has the address form" do
-        pending "needs to be implemented"
+        expect(address_form).to be_present
       end
 
       context "in the address form" do
-        it "has a required field for a street address" do
-          pending "needs to be implemented"
+        it "has a required field for the address line 1" do
+          expect(address_form.css('label[for="address_line_1"]')).to be_present
+          expect(address_form.css('input[type="text"][name="address_line_1"]'\
+                                  '[id="address_line_1"][required="required"]')).to be_present
         end
 
-        it "has a required field for a zip code" do
-          pending "needs to be implemented"
+        it "has a required field for the city" do
+          expect(address_form.css('label[for="city"]')).to be_present
+          expect(address_form.css('input[type="text"][name="city"]'\
+                                  '[id="city"][required="required"]')).to be_present
         end
 
-        it "has a required field for a city" do
-          pending "needs to be implemented"
+        it "has a required field for the state" do
+          expect(address_form.css('label[for="state"]')).to be_present
+          expect(address_form.css('input[type="text"][name="state"]'\
+                                  '[id="state"][required="required"]')).to be_present
         end
 
-        it "has a required field for a country" do
-          pending "needs to be implemented"
+        it "has a required field for the zip code" do
+          expect(address_form.css('label[for="zip_code"]')).to be_present
+          expect(address_form.css('input[type="text"][name="zip_code"]'\
+                                  '[id="zip_code"][required="required"]')).to be_present
+        end
+
+        it "has a required field for the country" do
+          expect(address_form.css('label[for="country"]')).to be_present
+          expect(address_form.css('input[type="text"][name="country"]'\
+                                  '[id="country"][required="required"]')).to be_present
         end
 
         it "has a submit" do
-          pending "needs to be implemented"
+          expect(address_form.css('input[type="submit"][name="commit"]'\
+                                  '[value="Search"][data-disable-with="Search"]')).to be_present
         end
       end
     end
 
     shared_examples "a page with weather forecast details" do
+      let(:weather_forecast_details_section) do
+        parsed_html_body.css('div[test_id="weather_forecast_details"]')
+      end
+
       it "has the weather forecast details section" do
-        pending "needs to be implemented"
+        expect(weather_forecast_details_section).to be_present
       end
 
       context "in the weather forecast details section" do
         it "has the current temperature" do
-          pending "needs to be implemented"
+          expect(weather_forecast_details_section.text).to include("Current: 89F")
         end
 
         it "has the high temperature for the day" do
-          pending "needs to be implemented"
+          expect(weather_forecast_details_section.text).to include("High: 95F")
         end
 
         it "has the low temperature for the day" do
-          pending "needs to be implemented"
+          expect(weather_forecast_details_section.text).to include("Low: 82F")
         end
 
         it "has the extended forecast" do
@@ -73,6 +100,16 @@ RSpec.describe "Weather", type: :request do
     end
 
     context "when an address is specified in the params" do
+      let(:params) do
+        {
+          address_line1: '123 Main St',
+          city: 'Santa Ana',
+          state: 'CA',
+          zip_code: '92701',
+          country: 'US'
+        }
+      end
+
       context "when the weather forecast data is cached for the given zip code" do
         it_behaves_like "a page with an address form"
         it_behaves_like "a page with weather forecast details"
@@ -93,6 +130,8 @@ RSpec.describe "Weather", type: :request do
     end
 
     context "when no address is specified in the params" do
+      let(:params) { {} }
+
       it_behaves_like "a page with an address form"
     end
   end
